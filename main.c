e65b55da91abcb07dc6438c23c533068a41250da
Mads Hartwich <s235102@dtu.dk>
2024-09-29 10:56:15 +0200

    replaces main file

--- main.c
+++ main.c
@@ -1,8 +1,149 @@
+/* You are not allowed to use <stdio.h> */
 #include "io.h"
+#include <stdlib.h>
 
-char msg[] = "\n ** You need to copy your main.c file from Assignment 1 **\n";
+typedef struct Node {
+    int value;
+    struct Node* next;
+    struct Node* prev;
+}Node;
 
-int main(int argc, char ** argv) {
-  write_string(msg);
-  return -1;
+typedef struct List {
+    Node *head;
+    Node *tail;
+    int Count;
+}List;
+
+void freeList (List* list) {
+    Node *currentNode;
+    while(list->head != NULL) {
+        currentNode = list->head;
+        list->head = list->head->next;
+        free(currentNode);
+    }
+    free(list);
+}
+
+Node* initNode(int value) {
+    Node* tempNode = (Node*) malloc(sizeof(Node));
+    tempNode->value = value;
+    tempNode->next = NULL;
+    tempNode->prev = NULL;
+    return tempNode;
+}
+
+void insertNodeAtEnd(Node** head, int value) {
+    Node* newNode = initNode(value);
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node* temp = *head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        write_int(temp->value);
+        if(temp->next != NULL) write_char(',');
+        temp = temp->next;
+    }
+    write_char(';');
+}
+/**
+ *
+ * @param head head of a doubly linked list
+ * @return the new head
+ */
+void deleteFromEnd(Node** head) {
+    if (*head == NULL) return;
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    if (temp->prev != NULL) {
+        temp->prev->next = NULL;
+    } else {
+        *head = NULL;
+    }
+    free(temp);
+}
+
+/**
+ * @name  main
+ * @brief This function is the entry point to your program
+ * @return 0 for success, anything else for failure
+ *
+ *
+ * Then it has a place for you to implementation the command 
+ * interpreter as  specified in the handout.
+ */
+int
+main()
+{
+  /*-----------------------------------------------------------------
+   *TODO:  You need to implement the command line driver here as
+   *       specified in the assignment handout.
+   *
+   * The following pseudo code describes what you need to do
+   *  
+   *  Declare the counter and the collection structure variables
+   *
+   *
+   *  In a loop
+   *    1) Read a command from standard in using read_char function
+   *    2) If the command is not 'a', 'b', 'c': then break the loop
+   *    3) Process the command as specified in the handout
+   *  End loop
+   *
+   *  Print your collection of elements as specified in the handout
+   *    as a comma delimited series of integers
+   *-----------------------------------------------------------------*/
+
+  int count = 0;
+  Node *head = NULL;
+  char * prompt = "Press q then return to quit\n";
+
+ // write_string(prompt);
+
+  char c;
+  do {
+      c = read_char();
+      if(c < 0) {
+          break;
+      }
+      if(c == 'a') {
+          insertNodeAtEnd(&head, count);
+          count++;
+      }
+      if(c == 'b') {
+          count++;
+      }
+      if(c == 'c') {
+          deleteFromEnd(&head);
+          count++;
+      }
+  } while(c == 'a' || c == 'b' || c == 'c');
+
+    printList(head);
+    write_char('\n');
+/*
+  if(c == 'q') {
+
+      //write_string("Collection: ");
+      printList(head);
+      write_char('\n');
+  } else {
+      write_string("ERROR");
+      write_char('\n');
+      return 1;
+  } */
+
+  return 0;
 }
